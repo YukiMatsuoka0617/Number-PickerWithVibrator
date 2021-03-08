abb06fb770df8c4b53483abe5e5ce8b6c282c1fa
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2021-03-09 01:54:54 +0900

    Add exception handling

--- app/src/main/java/com/example/numberpicker/MainActivity.java
+++ app/src/main/java/com/example/numberpicker/MainActivity.java
@@ -11,17 +11,21 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnVa
 
     Vibrator vibrator;
 
+    NumberPicker numberPicker0;
+    NumberPicker numberPicker1;
+    NumberPicker numberPicker2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        NumberPicker numberPicker = findViewById(R.id.hour);
-        NumberPicker numberPicker1 = findViewById(R.id.numPicker1);
-        NumberPicker numberPicker2 = findViewById(R.id.numPicker2);
+        numberPicker0 = findViewById(R.id.hour);
+        numberPicker1 = findViewById(R.id.numPicker1);
+        numberPicker2 = findViewById(R.id.numPicker2);
 
-        numberPicker.setMaxValue(24);
-        numberPicker.setMinValue(0);
+        numberPicker0.setMaxValue(23);
+        numberPicker0.setMinValue(0);
 
         numberPicker1.setMaxValue(5);
         numberPicker1.setMinValue(0);
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnVa
         numberPicker2.setMaxValue(9);
         numberPicker2.setMinValue(0);
 
-        numberPicker.setOnValueChangedListener(this);
+        numberPicker0.setOnValueChangedListener(this);
         numberPicker1.setOnValueChangedListener(this);
         numberPicker2.setOnValueChangedListener(this);
 
@@ -38,9 +42,38 @@ public class MainActivity extends AppCompatActivity implements NumberPicker.OnVa
 
     @Override
     public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-        Log.d("test","NumberPicker" + numberPicker.getId());
-        Log.d("test","i:" + i);
-        Log.d("test","i1:" + i1);
         vibrator.vibrate(50);
+        switch (numberPicker.getId()) {
+            case R.id.hour:
+                Log.d("test", "getValue:" + numberPicker.getValue());
+//                break;
+            case R.id.numPicker1:
+                Log.d("test", "getValue:" + numberPicker.getValue());
+                break;
+            case R.id.numPicker2:
+                if (i == 9 && i1 == 0) {
+                    if (numberPicker1.getValue() == 5) {
+                        numberPicker0.setValue(numberPicker0.getValue() + 1);
+                        numberPicker1.setValue(0);
+                    } else {
+                        numberPicker1.setValue(numberPicker1.getValue() + 1);
+                    }
+                }
+
+                if (i == 0 && i1 == 9) {
+                    if(numberPicker0.getValue() != 0 && numberPicker1.getValue() == 0) {
+                        numberPicker0.setValue(numberPicker0.getValue() - 1);
+                        numberPicker1.setValue(5);
+                    }
+                    else if(numberPicker0.getValue() == 0 && numberPicker1.getValue() == 0){
+                        numberPicker0.setValue(numberPicker0.getValue() - 1);
+                        numberPicker1.setValue(numberPicker1.getValue() - 1);
+                    }
+                    else {
+                        numberPicker1.setValue(numberPicker1.getValue() - 1);
+                    }
+                }
+                break;
+        }
     }
 }
